da2ebfc7a5d88941c9ebac25814feb4eb1ca2164
tyufjvbn2 <tyufjvbn22@gamil.com>
2021-05-18 12:47:10 +0900

    feat: 회원가입, 로그인 기본기능 구현, bcrypt, jwt, 쿠키적용

--- src/controllers/users/login.ts
+++ src/controllers/users/login.ts
@@ -1,5 +1,44 @@
 import { Request, Response } from "express";
+const users = require("../../models/collection/user");
+const jwt = require("jsonwebtoken")
+const bcrypt = require("bcrypt")
+
 
 module.exports = async (req: Request, res: Response) => {
-  res.status(200).json({ message: "login" });
+  const {id, password} = req.body
+  
+  //유저 패스워드 가져오기
+  const userInfo = await users.findOne({
+    id: id
+  })
+
+  // console.log(userInfo)
+  
+  if(!userInfo) {
+    res.status(403).json({message: "There is no user information"})
+  }  
+  else {
+    //패스워드 비교하기
+    const verified = await bcrypt.compare(password, userInfo.password)
+    if(!verified) {
+      res.status(401).json({message: "You wrote wrong password"})
+    }
+    else {
+      const {id, username, email, admin, social} = userInfo
+      
+      const payload = {
+        id, username, email, admin, social
+      }
+
+      //만료 기간은 추후 수정 예정
+      const token = jwt.sign(payload, process.env.SALT, {expiresIn: "1d"})
+
+      //클라이언트와 이야기해보기
+      res.cookie("authorization", token)
+
+
+      res.status(200).json({message: "Login Succeed", data : payload})
+    }
+  }
+
 };
---
+++ src/middleware/jwt.ts
@@ -0,0 +1,14 @@
+//토큰에 대한 처리만 진행
+const jwt = require("jsonwebtoken")
+
+jwt.verify()
+//header authorization
+//  있지만 토큰이 다른 토큰
+//  맞는데 expire된것
+//  통과
+
+
+//토큰 생성
+//  로그인
+//토큰 검증
+//  나머지 서비스
