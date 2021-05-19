f9691e219c5e3520a4898259f7ebd84a7f88722e
tyufjvbn2 <tyufjvbn22@gamil.com>
2021-05-19 11:47:50 +0900

    feat: 문의 작성 기능 구현

--- src/controllers/users/addquestion.ts
+++ src/controllers/users/addquestion.ts
@@ -1,5 +1,17 @@
 import { Request, Response } from "express";
+const users = require("../../models/collection/user");
 
 module.exports = async (req: Request, res: Response) => {
-  res.status(200).json({ message: "addquestion" });
+  try{
+    const {id} = req.body.userData
+    const {category, title, contents} = req.body
+
+    await users.update({id: id}, {$push: {category, title, contents}})
+
+
+    res.status(200).json({ message: "addquestion" });
+
+  } catch(err) {
+    console.error(err)
+  }
 };
