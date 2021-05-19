ac50529bf3e930cde3988d37b5b10d579a520322
Dodo9128 <wodbs9128@gmail.com>
2021-05-19 10:57:28 +0900

    fix: 회원탈퇴 로직 jwt 미들웨어 적용해서 수정, updateinfo, jwt 추가 수정

--- src/controllers/users/updateinfo.ts
+++ src/controllers/users/updateinfo.ts
@@ -4,10 +4,10 @@ const users = require("../../models/collection/user");
 
 module.exports = async (req: Request, res: Response) => {
   if (!req.body.userData) {
+    // 인증이 완료되지 않았을 때
     res.status(401).json({ message: "Wrong Authorization" });
   } else {
     const verified = await compare(req.body.oldpassword, req.body.userData.id);
-
     if (!verified) {
       res.status(401).json({ message: "Wrong password" });
     } else {
@@ -26,32 +26,33 @@ module.exports = async (req: Request, res: Response) => {
         // oldPw !== newPw
         if (!sameEmailCheck) {
           // 겹치는 이메일 없는 상태
-
           const currentUser = await users.findOne({ id: req.body.userData.id });
 
-          let changeUsername;
-          let changeEmail;
-          let changePw;
+          let changeUsername; // 바꿀 닉네임
+          let changeEmail; // 바꿀 이메일
+          let changePw; // 바꿀 패스워드
 
           if (!requestUsername) {
+            // 바꿀 닉네임 입력 X
             changeUsername = currentUser.username;
           } else {
+            // 바꿀 닉네임 입력
             changeUsername = requestUsername;
           }
-
           if (!requestEmail) {
+            // 바꿀 이메일 입력 X
             changeEmail = currentUser.email;
           } else {
+            // 바꿀 이메일 입력
             changeEmail = requestEmail;
           }
-
           if (!requestPw) {
+            // 바꿀 패스워드 있을 때
             changePw = currentUser.password;
           } else {
+            // 바꿀 패스워드 입력
             changePw = encrypt(requestPw);
           }
-          console.log(changeUsername, changeEmail, changePw);
-
           await users.update(
             { id: req.body.userData.id },
             {
@@ -60,16 +61,18 @@ module.exports = async (req: Request, res: Response) => {
               password: changePw,
             },
           );
-
           res.status(200).json({ message: "Update info" });
         } else {
           if (sameEmailCheck.id === req.body.userData.id) {
+            // 수정하려는 이메일이 기존 이메일과 같음
             res.status(403).json({ message: "This is your Origin Email" });
           } else {
+            // 수정하려는 이메일을 이미 누군가 쓰고 있음
             res.status(403).json({ message: "This Email is already used" });
           }
         }
       } else {
+        // 수정하려는 비밀번호가 기존 비밀번호와 같음
         res.status(403).json({ message: "You already use this password" });
       }
     }
--- src/controllers/users/withdrawal.ts
+++ src/controllers/users/withdrawal.ts
@@ -1,29 +1,24 @@
 import { Request, Response } from "express";
 const users = require("../../models/collection/user");
-const jwt = require("jsonwebtoken");
 
 module.exports = async (req: Request, res: Response) => {
   try {
-    const authorization = req.headers.authorization;
-    if (authorization) {
-      const token = authorization.split(" ")[1];
-      const decoded = jwt.verify(token, process.env.SALT);
-      const user = await users.findOneAndDelete({ id: decoded.id });
-      if (!user) {
-        return res.status(404).json({
-          message: "User not found",
-        });
-      } else {
-        res.clearCookie("authorization", { path: "/" });
-        res.status(200).json({
-          message: "withdrawal done",
-        });
-      }
+    const decodedData = req.body.userData;
+    const user = await users.findOneAndDelete({ id: decodedData.id });
+    if (!user) {
+      return res.status(404).json({
+        message: "User not found",
+      });
+    } else {
+      res.clearCookie("authorization", { path: "/" });
+      res.status(200).json({
+        message: "withdrawal done",
+      });
+      console.log("탈퇴 완료");
     }
   } catch (error) {
     res.status(500).json({
-      success: false,
-      data: "server error",
+      message: "server error",
     });
   }
 };
