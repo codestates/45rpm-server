0412bcd09e2196e2048d9874d8b66b0335fef0dc
tyufjvbn2 <tyufjvbn22@gamil.com>
2021-05-20 11:00:20 +0900

    feat:문의 수정 기능구현중

--- src/controllers/users/questionupdate.ts
+++ src/controllers/users/questionupdate.ts
@@ -6,15 +6,26 @@ module.exports = async (req: Request, res: Response) => {
     const {id} = req.body.userData
     const {questionId, category, title, contents} = req.body
 
-    const userDocument = await Users.find({id: id})
+    // const questionDocument = await Users.findOne({id: id}, (err:any, user:any) =>{
+    //   const targetQuestion = user.question.filter((question:any) => {
+    //     return question._id === questionId
+    //   }).pop();
+    // })
 
-    const updating = await userDocument.findById({questionId})
+    const userDocument = await Users.findOne({id: id})
 
-    console.log("찾은 객체", updating)
-    // await Users.findByIdAndUpdate(
-    //   questionId, 
-    //   {category, title, contents}
-    //   )
+
+    console.log("찾은 유저", userDocument)
+    const targetQuestion = userDocument.question.filter((question:any) => {
+      return question._id.toString() === questionId
+    })[0]
+    console.log("찾은 객체", targetQuestion)
+
+    targetQuestion.category = category
+    targetQuestion.title = title
+    targetQuestion.contents = contents
+
+    console.log("수정후 객체", targetQuestion)
 
 
     res.status(200).json({ message: "Your question is successfully updated" });
