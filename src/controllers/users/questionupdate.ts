9bee8eb067d7d06afb85c78fc4dffd810eebc07e
tyufjvbn2 <tyufjvbn22@gamil.com>
2021-05-20 11:35:12 +0900

    feat:문의 수정 기능 구현 완료

--- src/controllers/users/questionupdate.ts
+++ src/controllers/users/questionupdate.ts
@@ -6,27 +6,33 @@ module.exports = async (req: Request, res: Response) => {
     const {id} = req.body.userData
     const {questionId, category, title, contents} = req.body
 
-    // const questionDocument = await Users.findOne({id: id}, (err:any, user:any) =>{
-    //   const targetQuestion = user.question.filter((question:any) => {
-    //     return question._id === questionId
-    //   }).pop();
-    // })
-
+    //유저 도큐먼트를 먼저 찾음
     const userDocument = await Users.findOne({id: id})
 
-
     console.log("찾은 유저", userDocument)
+
+    //원하는 서브 도큐먼트를 필터링 해서 찾음
     const targetQuestion = userDocument.question.filter((question:any) => {
       return question._id.toString() === questionId
     })[0]
+
     console.log("찾은 객체", targetQuestion)
+    
+    //서브 도큐먼트의 인덱스도 찾음
+    const targetQuestionIdx = userDocument.question.indexOf(targetQuestion)
+    console.log("찾은 객체 인덱스값", targetQuestionIdx)
 
-    targetQuestion.category = category
-    targetQuestion.title = title
-    targetQuestion.contents = contents
+    //인덱스를 바탕으로 값 수정(ObjectId는 변경됨)
+    userDocument.question.set(targetQuestionIdx, {
+      category: category,
+      title: title,
+      contents: contents
+    })
 
-    console.log("수정후 객체", targetQuestion)
+    console.log("수정후 객체", userDocument.question[targetQuestionIdx])
 
+    userDocument.save()
+    console.log("문의 수정 완료!")
 
     res.status(200).json({ message: "Your question is successfully updated" });
 
