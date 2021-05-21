aeedf3bc5d8582d57645437bcf09cd0fd6fb7b75
tyufjvbn2 <tyufjvbn22@gamil.com>
2021-05-21 16:03:02 +0900

    feat: 주문 기능 구현 완료

--- src/controllers/users/addcustomorder.ts
+++ src/controllers/users/addcustomorder.ts
@@ -1,5 +1,14 @@
 import { Request, Response } from "express";
 
 module.exports = async (req: Request, res: Response) => {
+  try{
+    console.log("리퀘 바디", req.body);
+    const { id } = req.body.userData;
+    const { userId, address, customerName, price, customId } = req.body;
+
+  } catch(err) {
+    console.error(err)
+  }
+
   res.status(200).json({ message: "addcustomorder" });
 };
