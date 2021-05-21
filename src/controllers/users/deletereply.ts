0d6504a4c5e7247e45b41903a6cf9919a9850122
tyufjvbn2 <tyufjvbn22@gamil.com>
2021-05-21 10:32:38 +0900

    feat: 답변 삭제 기능 구현

--- src/controllers/users/deletereply.ts
+++ src/controllers/users/deletereply.ts
@@ -2,6 +2,19 @@ import { Request, Response } from "express";
 
 module.exports = async (req: Request, res: Response) => {
   try {
+    const {admin} = req.body.userData
+    const {questionId} = req.body
+
+    if(admin === false) {
+      res.status(401).json({ message: "You don't have access" });
+    }
+    else {
+      
+    }
+
+
+
+
     res.status(200).json({ message: "deletereply" });
   } catch (err) {
     console.error(err);
