77c566016bf4bb2323f28b0dcd67a07576635b9d
tyufjvbn2 <tyufjvbn22@gamil.com>
2021-05-22 03:03:30 +0900

    feat:주문 추가 기능 구현 완료

--- src/controllers/users/addcustomorder.ts
+++ src/controllers/users/addcustomorder.ts
@@ -1,10 +1,23 @@
 import { Request, Response } from "express";
+const Customs = require("../../models/collection/Custom")
 
 module.exports = async (req: Request, res: Response) => {
   try{
     console.log("리퀘 바디", req.body);
     const { id } = req.body.userData;
-    const { userId, address, customerName, price, customId } = req.body;
+    const { address, customerName, price, customId } = req.body;
+
+    await Customs.findByIdAndUpdate(customId, {
+      $set: { 
+        order: { 
+          address: address,
+          customerName:customerName,
+          price: price
+        } 
+      },
+    });
+    res.status(200).json({ message: "Your custom saved succeed" });
+
 
   } catch(err) {
     console.error(err)
