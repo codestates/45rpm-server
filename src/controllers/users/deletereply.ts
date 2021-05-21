02800fa8f03bce075937cc14ece6547f3b4c207c
tyufjvbn2 <tyufjvbn22@gamil.com>
2021-05-22 02:45:51 +0900

    feat: 문의 답변 삭제 기능 구현 완료

--- src/controllers/users/deletereply.ts
+++ src/controllers/users/deletereply.ts
@@ -1,8 +1,19 @@
 import { Request, Response } from "express";
+const Question = require("../../models/collection/Question");
 
 module.exports = async (req: Request, res: Response) => {
   try {
-    res.status(200).json({ message: "deletereply" });
+    const {admin} = req.body.userData
+    const {questionId} = req.body
+
+    if(admin === false) {
+      res.status(401).json({ message: "You don't have access" });
+    }
+    else {
+      // admin 일 때
+      await Question.findByIdAndUpdate( questionId, { reply: null })
+      res.status(200).json({ message: "Reply is deleted" });
+    }
   } catch (err) {
     console.error(err);
     res.status(500).json({ message: err });
