48f5d6c6544f6c84507681636e3262465003529c
tyufjvbn2 <tyufjvbn22@gamil.com>
2021-05-22 18:47:41 +0900

    feat: 커스텀 삭제 기능 구현 완료

--- src/controllers/customs/delete.ts
+++ src/controllers/customs/delete.ts
@@ -1,5 +1,24 @@
 import { Request, Response } from "express";
+const Customs = require("../../models/collection/Custom");
+const Users = require("../../models/collection/User");
 
 module.exports = async (req: Request, res: Response) => {
-  res.status(200).json({ message: "delete" });
+  try{
+    const {customId} = req.body;
+    
+    const targetCustom = await Customs.findByIdAndRemove(customId);
+    
+    console.log("삭제 확인", targetCustom);
+
+    const userDoc = await Users.findOne({id: targetCustom.userId})
+
+    console.log("유저 정보", userDoc)
+
+    userDoc.customizeSet.pull(customId)
+    userDoc.save()
+
+    res.status(200).json({ message: "Your custom is successfully deleted" });
+  } catch(err) {
+    console.error(err)
+  }
 };
