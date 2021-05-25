3598d115ddf3758a95a1de8212b9399ac642dc6e
tyufjvbn2 <tyufjvbn22@gamil.com>
2021-05-25 23:31:49 +0900

    fix:카카오 로그인 통한 회원가입시 추가 정보 입력값 처리 수정

--- src/models/collection/User.ts
+++ src/models/collection/User.ts
@@ -7,6 +7,8 @@ const userSchema = new Schema(
     password: String,
     username: String,
     email: String,
+    profileUrl: String,
+    thumbnailUrl: String,
     admin: Boolean,
     social: Boolean,
     question: Array,
