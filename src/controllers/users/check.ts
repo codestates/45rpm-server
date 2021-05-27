a611818068d55b0c23b815dd58a5ac56e2dbaeb0
O Wooyoung <fz7948@gmail.com>
2021-05-28 07:12:47 +0900

    check 파일

---
+++ src/controllers/users/check.ts
@@ -0,0 +1,39 @@
+import { Request, Response } from "express";
+const Users = require("../../models/collection/User");
+const jwt = require("jsonwebtoken");
+import { compare } from "../controllermiddleware/bcrypt";
+
+module.exports = async (req: Request, res: Response) => {
+  console.log("body?????????????", req.body);
+  if (!req.body.id) {
+    return res.status(400).end();
+  }
+  const id = req.body.id;
+  try {
+    const data = await Users.findOne({ id: id });
+    if (!data) {
+      return res.json({ data: null, message: "no exist" });
+    }
+    // delete data.dataValues.password;
+
+    const payload = {
+      id: data.id,
+      username: data.username,
+      email: data.email,
+      admin: data.admin,
+      social: data.social,
+    };
+
+    const token = jwt.sign(payload, process.env.SALT, { expiresIn: "1d" });
+
+    res.cookie("authorization", token, {
+      path: "/",
+      domain: process.env.DOMAIN,
+    });
+
+    res.status(200).json({ message: "find data", data: data });
+  } catch (err) {
+    console.error(err);
+    res.status(401).json({ message: err });
+  }
+};
