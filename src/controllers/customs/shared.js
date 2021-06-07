18e94a38b3b5c5bbc797d5129b95bd7f0a0c1fe7
tyufjvbn2 <tyufjvbn22@gmail.com>
2021-06-07 23:11:18 +0900

    fix:공유페이지 LP갯수 9개 제한

--- src/controllers/customs/shared.js
+++ src/controllers/customs/shared.js
@@ -3,10 +3,12 @@ const Customs = require("../../models/collection/Custom");
 module.exports = async (req, res) => {
   try {
     // 최신 데이터부터 제공하기 위한 역 sort
-    const sharedCustom = await Customs.find({ share: true }).sort({
-      field: "desc",
-      _id: -1,
-    });
+    const sharedCustom = await Customs.find({ share: true })
+      .sort({
+        field: "desc",
+        _id: -1,
+      })
+      .limit(9);
 
     res.status(200).json({ message: "all shared custom", data: sharedCustom });
   } catch (err) {
